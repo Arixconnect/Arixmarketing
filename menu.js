b03bcaabc894f1b724396cecf4a1fe722fd69083
Arixconnect <arixoncamelia@gmail.com>
2026-08-27 10:34:11 +0200

    Load cookie consent on campaign pages

--- menu.js
+++ menu.js
@@ -1,3 +1,21 @@
+function loadCookieConsentAssets() {
+  if (!document.querySelector("link[href='/cookie-consent.css']")) {
+    const stylesheet = document.createElement("link");
+    stylesheet.rel = "stylesheet";
+    stylesheet.href = "/cookie-consent.css";
+    document.head.appendChild(stylesheet);
+  }
+
+  if (!document.querySelector("script[src='/cookie-consent.js']")) {
+    const script = document.createElement("script");
+    script.src = "/cookie-consent.js";
+    script.defer = true;
+    document.head.appendChild(script);
+  }
+}
+
+loadCookieConsentAssets();
+
 const header = document.querySelector(".site-header");
 const menuToggle = document.querySelector(".menu-toggle");
 const navLinks = document.querySelectorAll(".nav a, .header-cta");
